1fca326abdc33181f988f18b92eeb2de56657d9b
竹下陽喜 <>
2024-07-15 15:09:58 +0900

    no message

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx}', './public/index.html'],
+  darkMode: 'media', 
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
