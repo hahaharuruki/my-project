981c9b1942391d6767520cb092ec5c320b65a28d
竹下陽喜 <>
2024-07-18 00:44:42 +0900

    modified config

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/my-project/'
+    ? '/'
     : '/',
   configureWebpack: {
     resolve: {
